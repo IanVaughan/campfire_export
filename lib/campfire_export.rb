9ef42e3c355f378459f3f06c95a0df71c5189c94
Ian Vaughan <github@ianvaughan.co.uk>
2013-11-11 09:04:11 +0000

    move file load path project

--- lib/campfire_export.rb
+++ lib/campfire_export.rb
@@ -1,4 +1,5 @@
-require 'campfire_export/timezone'
+lib = File.expand_path(File.join(File.dirname(__FILE__), '../lib'))
+$LOAD_PATH.unshift(lib) if File.directory?(lib) && !$LOAD_PATH.include?(lib)
 
 require 'cgi'
 require 'fileutils'
@@ -6,6 +7,11 @@ require 'httparty'
 require 'nokogiri'
 require 'time'
 require 'yaml'
+
+require './lib/campfire_export/io.rb'
+
+Dir[File.dirname(__FILE__) + '/campfire_export/*.rb'].each {|file| require file }
+
 
 module CampfireExport
 end
